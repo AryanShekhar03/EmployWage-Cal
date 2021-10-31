48a6eed36ea9bdd3304ee531cc9c812d7fcd4056
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-10-31 15:18:32 +0530

    First commit

---
+++ UC1-Employee attendance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC1_Employee_attendance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+         {
+            int isFullTime = 1;
+
+            Random random = new Random();
+
+           int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+        }
+    }
+}
---
+++ uc-2EmployeeWage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isFulltime = 1;
+            int empRatePerHours = 20;
+
+            int empHrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+
+            int empcheck = random.Next(0, 2);
+            if (empcheck == isFulltime)
+            {
+                //Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                //console.WriteLine("Employee is not present");
+                empHrs = 0;
+            }
+            empwage = empHrs * empRatePerHours;
+            Console.WriteLine("Employee Days wage:" + empwage);
+        }
+    }
+}
+
+            
+
+            
