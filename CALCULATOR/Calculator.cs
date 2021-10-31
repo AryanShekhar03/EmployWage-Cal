7526ed56ff33133ee7c63b0f0156849d430acbd8
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-10-31 15:06:33 +0530

    Add Calculator

---
+++ CALCULATOR/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CALCULATOR
+{
+    public class Calculator
+    {
+
+        int totalValue;
+
+        public int isSum(int num1, int num2)
+        {
+
+            return totalValue = num1 + num2;
+        }
+    }
+}
---
+++ CALCULATOR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CALCULATOR
+{
+   public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Calculator ojccalculator = new Calculator();
+            Console.WriteLine("total values=" + ojccalculator.isSum(40, 20));
+            Console.ReadKey();
+        }
+    }
+}
