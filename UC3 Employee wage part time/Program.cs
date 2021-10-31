d9b20cf0fece111a5b883671d5993c1990496da6
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-10-31 13:42:39 +0530

    First Commit

---
+++ UC3 Employee wage part time/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UC3_Employee_wage_part_time
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+         {
+            int isFullTime = 2;
+            int isPartTime = 1;
+            int empRatePerHours = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPartTime)
+            {
+                empHrs = 4;
+            }
+            if (empCheck == isFullTime)
+            {
+                //console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else
+            {
+                //console.WriteLine("Employee is not present");
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHours;
+            Console.WriteLine("Employee Days Wage:" + empWage);        
+        }
+    }
+}
